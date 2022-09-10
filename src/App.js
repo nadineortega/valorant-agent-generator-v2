5292a40e2d8ed78cd891a9765499e7105e59e4a1
nadin3amanda <nadine@soar.earth>
2022-09-10 21:01:27 +0800

    fixes packages and modifies files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Valorant Agent Generator</h1>
+      <p>Click button to randomly select an agent to use for your current game</p>
+      <button>Generate Now</button>
     </div>
   );
 }
