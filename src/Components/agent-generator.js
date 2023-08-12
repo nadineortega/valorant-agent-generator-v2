57e6a90fee42ce50787982fe98ab898ef015c925
nadin3amanda <nadineamanduh@gmail.com>
2023-08-12 11:27:42 +0800

    added agents on list

--- src/Components/agent-generator.js
+++ src/Components/agent-generator.js
@@ -1,64 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 
-
-
 const RandomAgentGenerator = () => {
+  function getRandomAgent(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  const [agent, setAgent] = React.useState(0);
+  const images = [
+    require("../Images/valorant-agents/astra.png"),
+    require("../Images/valorant-agents/breach.png"),
+    require("../Images/valorant-agents/brimstone.png"),
+    require("../Images/valorant-agents/chamber.png"),
+    require("../Images/valorant-agents/cypher.png"),
+    require("../Images/valorant-agents/deadlock.png"),
+    require("../Images/valorant-agents/fade.png"),
+    require("../Images/valorant-agents/harbor.png"),
+    require("../Images/valorant-agents/jett.png"),
+    require("../Images/valorant-agents/killjoy.png"),
+    require("../Images/valorant-agents/neon.png"),
+    require("../Images/valorant-agents/omen.png"),
+    require("../Images/valorant-agents/phoenix.png"),
+    require("../Images/valorant-agents/raze.png"),
+    require("../Images/valorant-agents/reyna.png"),
+    require("../Images/valorant-agents/sage.png"),
+    require("../Images/valorant-agents/sova.png"),
+    require("../Images/valorant-agents/viper.png"),
+    require("../Images/valorant-agents/yoru.png"),
+  ];
 
-    function getRandomAgent(min,max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
-    const [agent, setAgent] = React.useState(0);
-    const images = [
-        require('../Images/valorant-agents/astra.png'),
-        require('../Images/valorant-agents/breach.png'),
-        require('../Images/valorant-agents/brimstone.png'),
-        require('../Images/valorant-agents/chamber.png'),
-        require('../Images/valorant-agents/cypher.png'),
-        require('../Images/valorant-agents/fade.png'),
-        require('../Images/valorant-agents/jett.png'),
-        require('../Images/valorant-agents/killjoy.png'),
-        require('../Images/valorant-agents/neon.png'),
-        require('../Images/valorant-agents/omen.png'),
-        require('../Images/valorant-agents/phoenix.png'),
-        require('../Images/valorant-agents/raze.png'),
-        require('../Images/valorant-agents/reyna.png'),
-        require('../Images/valorant-agents/sage.png'),
-        require('../Images/valorant-agents/sova.png'),
-        require('../Images/valorant-agents/viper.png'),
-        require('../Images/valorant-agents/yoru.png'),
-    ];
+  const handleClick = () => {
+    const randomAgentImage = getRandomAgent(0, images.length - 1);
 
-    const handleClick = () => {
-        const randomAgentImage = getRandomAgent(0, images.length -1);
+    setAgent(randomAgentImage);
+  };
 
-        setAgent(randomAgentImage);
-    };
+  const currentAgent = images[agent];
 
-    const currentAgent = images[agent];
-
-return (
+  return (
     <>
-    <button className="generate-btn" onClick={handleClick}> get agent</button>
-    <RandomGenerator>
+      <button className="generate-btn" onClick={handleClick}>
+        {" "}
+        get agent
+      </button>
+      <RandomGenerator>
         <img src={currentAgent} alt="valorant-agent" width="350" height="680" />
-    </RandomGenerator>
+      </RandomGenerator>
     </>
-    );
-}
-
+  );
+};
 
 export default RandomAgentGenerator;
 
 const RandomGenerator = styled.div`
-    width: 380px;
-    height: 800px;
-    margin: 0 auto;
-    display: block;
-    padding-top: 20px;
+  width: 380px;
+  height: 800px;
+  margin: 0 auto;
+  display: block;
+  padding-top: 20px;
 
-    @media only screen and (max-width: 600px) {
-        width: 70vw;
-        height: 550px;
-    }
+  @media only screen and (max-width: 600px) {
+    width: 70vw;
+    height: 550px;
+  }
 `;
